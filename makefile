7c14957573da1fdeb3f7eeeb4895973544319c84
Kirill Ivanchin <chvik1@yandex.ru>
2025-02-08 14:54:04 +0300

    updated pinger service

---
+++ makefile
@@ -0,0 +1,4 @@
+all: docker_compose_up
+
+docker_compose_up:
+	docker-compose up
--- pinger/cmd/main/main.go
+++ pinger/cmd/main/main.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"io/ioutil"
 	"log"
@@ -23,35 +24,23 @@ type dockerContainer struct {
 }
 
 type PingResult struct {
-	IP       string `json:"ip"`        // IP-адрес контейнера
-	PingTime int    `json:"ping_time"` // Время пинга в миллисекундах
-	Success  bool   `json:"success"`   // Успешен ли pingfunc
+	IP       string `json:"ip"`        // ip-адрес контейнера
+	PingTime int    `json:"ping_time"` // продолжительность пинга в миллисекундах
+	Success  bool   `json:"success"`   // успешен ли pingfunc
 }
-
-//func pingfunc(ip string) (int, bool) {
-//	start := time.Now()                        // Засекаем время начала
-//	cmd := exec.Command("pingfunc", "-n", "1", ip) // Используем "-n" для Windows
-//
-//	_, err := cmd.CombinedOutput()
-//	if err != nil {
-//		fmt.Printf("Ошибка: %s\n", err)
-//	}
-//
-//	return int(time.Since(start).Milliseconds()), true // Возвращаем время и успешный статус
-//}
 
 func pingfunc(ip string) (int, bool) {
 	pinger, err := ping.NewPinger(ip)
 	if err != nil {
-		fmt.Printf("Ошибка при создании Pinger: %s\n", err)
+		log.Printf("Ошибка при создании Pinger: %s\n", err)
 		return 0, false
 	}
 
-	pinger.Count = 1
+	pinger.Count = 7
 	pinger.Timeout = time.Second * 5
 	err = pinger.Run()
 	if err != nil {
-		fmt.Printf("Ошибка при выполнении пинга: %s\n", err)
+		log.Printf("Ошибка при выполнении пинга: %s\n", err)
 		return 0, false
 	}
 
@@ -74,10 +63,10 @@ func takeDockerContainers() []dockerContainer {
 	}
 
 	var dockerContainers []dockerContainer
-	for _, container := range containers {
-		containerDetails, err := cli.ContainerInspect(context.Background(), container.ID)
+	for _, c := range containers {
+		containerDetails, err := cli.ContainerInspect(context.Background(), c.ID)
 		if err != nil {
-			log.Printf("Ошибка при получении деталей контейнера %s: %s", container.ID[:10], err)
+			log.Printf("Ошибка при получении деталей контейнера %s: %s", c.ID[:10], err)
 			continue
 		}
 
@@ -91,9 +80,9 @@ func takeDockerContainers() []dockerContainer {
 		}
 
 		dockerContainers = append(dockerContainers, dockerContainer{
-			Id:     container.ID,
-			Image:  container.Image,
-			Status: container.Status,
+			Id:     c.ID,
+			Image:  c.Image,
+			Status: c.Status,
 			Ip:     ipAddress,
 		})
 	}
@@ -105,7 +94,7 @@ func sendPingResult(result PingResult) {
 	postBody, _ := json.Marshal(result)
 	responseBody := bytes.NewBuffer(postBody)
 	resp, err := http.Post(
-		fmt.Sprintf("localhost:8080/v1/containers/%s", result.IP),
+		fmt.Sprintf("http://backend:8080/v1/containers/%s", result.IP),
 		"application/json",
 		responseBody,
 	)
@@ -122,26 +111,80 @@ func sendPingResult(result PingResult) {
 	log.Printf(sb)
 }
 
+func uniteConteiners() {
+	ctx := context.Background()
+
+	// Создаем клиент Docker
+	cli, err := client.NewClientWithOpts(
+		client.WithHost("tcp://host.docker.internal:2375"), // Используем TCP
+		client.WithAPIVersionNegotiation(),
+	)
+
+	// Получаем список всех контейнеров
+	containers, err := cli.ContainerList(context.Background(), container.ListOptions{})
+	if err != nil {
+		log.Fatalf("Ошибка при получении списка контейнеров: %s", err)
+	}
+
+	// Создаем новую сеть
+	networkName := "my_network"
+	network, err := cli.NetworkInspect(ctx, networkName, types.NetworkInspectOptions{})
+	if err == nil {
+		log.Printf("Сеть %s уже существует. Подключаем контейнеры к ней.\n", networkName)
+	} else {
+		// Если сети нет, создаем её
+		_, err = cli.NetworkCreate(ctx, networkName, types.NetworkCreate{})
+		if err != nil {
+			log.Fatalf("Ошибка создания сети: %v", err)
+		}
+		log.Printf("Сеть %s была создана.\n", networkName)
+	}
+
+	// Подключаем все контейнеры к созданной сети
+	for _, c := range containers {
+		// Проверяем подключение
+		isConnected := false
+		for k := range network.Containers {
+			if k == c.ID {
+				isConnected = true
+				break
+			}
+		}
+
+		if !isConnected {
+			// Подключаем контейнер к сети
+			err = cli.NetworkConnect(ctx, networkName, c.ID, nil)
+			if err != nil {
+				log.Printf("Ошибка подключения контейнера %s к сети %s: %v", c.ID, networkName, err)
+			} else {
+				log.Printf("Контейнер %s подключен к сети %s\n", c.ID, networkName)
+			}
+		} else {
+			log.Printf("Контейнер %s уже подключен к сети %s\n", c.ID, networkName)
+		}
+	}
+
+	log.Println("Все контейнеры были обработаны.")
+}
+
 func main() {
 	for {
+		uniteConteiners()
 		dockerContainers := takeDockerContainers()
 
 		for _, ip := range dockerContainers {
 			pingTime, success := pingfunc(ip.Ip) // Пингуем IP-адрес
-			//sendPingResult(PingResult{
-			//	IP:       ip.Ip,
-			//	PingTime: pingTime,
-			//	Success:  success,
-			//})
-			fmt.Printf("IP: %s, PingTime: %d ", ip.Ip, pingTime)
-			fmt.Printf("Success: %t\n", success)
+			sendPingResult(PingResult{
+				IP:       ip.Ip,
+				PingTime: pingTime,
+				Success:  success,
+			})
+
+			log.Printf("IP: %s, PingTime: %d Success: %t\n", ip.Ip, pingTime, success)
+			fmt.Println()
 		}
 
 		time.Sleep(10 * time.Second)
 	}
 
 }
-
-// написать конфиг и через него прокидывать адрес
-//docker build -t pings .
-// docker run -p 8081:8081 pings
